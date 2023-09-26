4b8e42b64c6022dcceac3f02e5c6dc53244660fe
andierm <adyrm98@gmail.com>
2023-09-26 23:12:02 +0200

    RandomColorChangers
    I made the lights to open randomly to red on start.

--- Assets/Scripts/MaterialChanger.cs
+++ Assets/Scripts/MaterialChanger.cs
@@ -7,20 +7,59 @@ public class MaterialChanger : MonoBehaviour
     public Material lightOff;
     public Material lightOn;
 
+    //testing the arrays
+    GameObject[] lights;
+    GameObject[] lightsOn;
+    GameObject selectedcube;
+    int index;
+    public int hitCounts = 0;
+    GameObject parentGameObject;
+    GameObject[] nextParentGameObject;
+
+    void Start()
+    {
+        lights = GameObject.FindGameObjectsWithTag("lights");
+        index = Random.Range(0, lights.Length);
+        selectedcube = lights[index];
+        //print(selectedcube.name);
+        selectedcube.GetComponent<MeshRenderer>().material= lightOn; //opening randomly some lights at the begining of the game
+        parentGameObject = this.gameObject.transform.parent.gameObject;
+    }
+
     void OnTriggerEnter(Collider other)
     {
-            if(GetComponent<MeshRenderer>().sharedMaterial == lightOff)
+
+        //not deleting it because i want it as reference
+        /*foreach (GameObject light in lights)
+        {
+            if (light.GetComponent<MeshRenderer>().sharedMaterial == lightOff)
             {
-               GetComponent<MeshRenderer>().sharedMaterial = lightOn;
+                light.GetComponent<MeshRenderer>().sharedMaterial = lightOn;
             }
 
             else
+            {
+               light.GetComponent<MeshRenderer>().sharedMaterial = lightOff;
+            }
+        }*/
+
+        if (GetComponent<MeshRenderer>().sharedMaterial == lightOff)
+        {
+            GetComponent<MeshRenderer>().sharedMaterial = lightOn;
+
+        }
+
+        else
         {
             GetComponent<MeshRenderer>().sharedMaterial = lightOff;
         }
 
-            
+       hitCounts++;
         
-        
+
+
     }
+
+
+    
 }

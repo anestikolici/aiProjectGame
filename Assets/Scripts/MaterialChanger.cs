c578e0509cc6602a609db7bcff152352c7ddf449
andierm <adyrm98@gmail.com>
2023-09-27 14:08:00 +0200

    LightLightsWorking

--- Assets/Scripts/MaterialChanger.cs
+++ Assets/Scripts/MaterialChanger.cs
@@ -15,46 +15,79 @@ public class MaterialChanger : MonoBehaviour
     public int hitCounts = 0;
     GameObject parentGameObject;
     GameObject[] nextParentGameObject;
+    GameObject[] cubes_1;
+    GameObject[] cubes_2;
+    GameObject[] cubes_3;
+
+
 
     void Start()
     {
-        lights = GameObject.FindGameObjectsWithTag("lights");
-        index = Random.Range(0, lights.Length);
-        selectedcube = lights[index];
+        //lights = GameObject.FindGameObjectsWithTag("lights"); //collect all cubes
+        //index = Random.Range(0, lights.Length); //specify some random cubes to be turned on at the start
+        //selectedcube = lights[index];
         //print(selectedcube.name);
-        selectedcube.GetComponent<MeshRenderer>().material= lightOn; //opening randomly some lights at the begining of the game
-        parentGameObject = this.gameObject.transform.parent.gameObject;
+        //selectedcube.GetComponent<MeshRenderer>().material= lightOn; //opening randomly some lights at the begining of the game
+        //parentGameObject = this.gameObject.transform.parent.gameObject;
+        cubes_1 = GameObject.FindGameObjectsWithTag("lights2");
+        print(cubes_1);
+
+       
+
+
+    }
+
+    void Update()
+    {
+
     }
 
     void OnTriggerEnter(Collider other)
     {
 
-        //not deleting it because i want it as reference
-        /*foreach (GameObject light in lights)
+       
+
+        for (int i=0;i<3;i++)
         {
-            if (light.GetComponent<MeshRenderer>().sharedMaterial == lightOff)
+
+            print(cubes_1[i].GetComponent<MeshRenderer>().sharedMaterial.name);
+            if (cubes_1[i].GetComponent<MeshRenderer>().sharedMaterial == lightOff)
             {
-                light.GetComponent<MeshRenderer>().sharedMaterial = lightOn;
+                cubes_1[i].GetComponent<MeshRenderer>().sharedMaterial = lightOn;
             }
 
             else
             {
-               light.GetComponent<MeshRenderer>().sharedMaterial = lightOff;
+                cubes_1[i].GetComponent<MeshRenderer>().sharedMaterial = lightOff;
             }
-        }*/
-
-        if (GetComponent<MeshRenderer>().sharedMaterial == lightOff)
-        {
-            GetComponent<MeshRenderer>().sharedMaterial = lightOn;
-
         }
 
-        else
-        {
-            GetComponent<MeshRenderer>().sharedMaterial = lightOff;
-        }
+        //not deleting it because i want it as reference
+        /*    foreach (GameObject light in lights)
+         {
+             if (light.GetComponent<MeshRebnderer>().sharedMaterial == lightOff)
+             {
+                 light.GetComponent<MeshRenderer>().sharedMaterial = lightOn;
+             }
 
-       hitCounts++;
+             else
+             {
+                light.GetComponent<MeshRenderer>().sharedMaterial = lightOff;
+             }
+         }
+
+         /*if (GetComponent<MeshRenderer>().sharedMaterial == lightOff)
+         {
+             GetComponent<MeshRenderer>().sharedMaterial = lightOn;
+
+         }
+
+         else
+         {
+             GetComponent<MeshRenderer>().sharedMaterial = lightOff;
+         }*/
+
+        hitCounts++;
         
 
 

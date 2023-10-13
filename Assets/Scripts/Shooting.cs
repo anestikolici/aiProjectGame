7ac9c3b6b3cdac5d6eeee80f7b22b8d05a406c55
anestikolici <85286176+anestikolici@users.noreply.github.com>
2023-10-13 18:59:21 +0200

    recoil

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -7,11 +7,21 @@ public class Shooting : MonoBehaviour
     public Transform bulletSpawnPoint;
     public GameObject bulletPrefab;
     public float bulletSpeed = 10;
+    
+    private ProceduralRecoil recoilComponent;
 
+    void Start()
+    {
+        recoilComponent = GameObject.Find("Rifle AK").GetComponent<ProceduralRecoil>();
+    }
+
+    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            recoilComponent.StartRecoil(0.2f, 10f, 10f);
+
             var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
             bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
         }
